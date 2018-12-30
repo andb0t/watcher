47c1fd41f79bc5316f1b4fdfe308b9a282b757cf
Andreas Alexander Maier <andb0t@pm.me>
2018-12-30 18:41:37 +0100

    Delete go.mod

--- watcher.go
+++ watcher.go
@@ -1,16 +1,38 @@
 package main
 
-import "fmt"
+import (
+  "fmt"
+  "gopkg.in/yaml.v2"
+  "io/ioutil"
+  "log"
+  )
 
+
+type conf struct {
+    Keywords []string `yaml:"keywords"`
+    Vetowords []string `yaml:"vetowords"`
+    Websites []string `yaml:"websites"`
+}
+
+func (c *conf) getConf() *conf {
+    yamlFile, err := ioutil.ReadFile("conf.yaml")
+    if err != nil {
+        log.Printf("yamlFile.Get err   #%v ", err)
+    }
+    err = yaml.Unmarshal(yamlFile, c)
+    if err != nil {
+        log.Fatalf("Unmarshal: %v", err)
+    }
+    return c
+}
 
 func main() {
+    var c conf
+    c.getConf()
     fmt.Println("Print checking for offers ...")
-    keywords := []string {"Gebrauchtwagen", "BMW", "Audi"}
-    vetowords := []string {"Neuwagen"}
-    websites := []string {"www.ebay.de", "www.ebay-kleinanzeigen.de"}
-    fmt.Println("Looking for:", keywords)
-    fmt.Println(" ... while vetoing:", vetowords)
-    fmt.Println(" ... on:", websites)
+    fmt.Println("Looking for:", c.Keywords)
+    fmt.Println(" ... while vetoing:", c.Vetowords)
+    fmt.Println(" ... on:", c.Websites)
 
     RequestEbay()
 }
