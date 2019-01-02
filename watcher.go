62154d3ae506cfbd724e4aaaa47fa8ba316577c1
Andreas Alexander Maier <andb0t@pm.me>
2019-01-02 15:59:03 +0100

    Apply go fmt

--- watcher.go
+++ watcher.go
@@ -47,10 +47,10 @@ func main() {
 }
 
 func contains(s []string, e string) bool {
-    for _, a := range s {
-        if a == e {
-            return true
-        }
-    }
-    return false
+	for _, a := range s {
+		if a == e {
+			return true
+		}
+	}
+	return false
 }
